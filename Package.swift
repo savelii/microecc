920a8cf60e06791317dc69d33db947c8be5d9548
Saveliy Stavitsky <s.stavitsky@icloud.com>
2022-01-28 18:19:02 +0200

    test

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "microecc",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "microecc",
+            targets: ["microecc"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "microecc",
+            dependencies: [])
+    ]
+)
--- eosswiftTests/eos-http-rpc/Config.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-class Config {
-    static let CHAIN_API_BASE_URL = "https://api.jungle.alohaeos.com:443/"
-    static let HISTORY_API_BASE_URL = "https://api.jungle.alohaeos.com:443/"
-    static let MAIN_NET_API_BASE_URL = "https://eos.greymass.com/"
-}
