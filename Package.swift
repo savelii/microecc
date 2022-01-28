fd1ba15105b4050c85b48a1fc1ce2ce38a7d7bf6
Saveliy Stavitsky <s.stavitsky@icloud.com>
2022-01-28 19:14:51 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,9 @@ import PackageDescription
 
 let package = Package(
     name: "microecc",
+    platforms: [
+        .iOS(.v9)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -16,6 +19,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .systemLibrary(
-            name: "microecc")
+            name: "microecc"
+        )
     ]
 )
