fd92739a0df7ff91292ec9a475fed8953597c0b9
Saveliy Stavitsky <s.stavitsky@icloud.com>
2022-01-28 18:48:52 +0200

    test3

--- Package.swift
+++ Package.swift
@@ -16,7 +16,6 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .systemLibrary(
-            name: "microecc",
-            dependencies: [])
+            name: "microecc")
     ]
 )
