310ae55fab2d8e421ed0e77d0d3b780c779ae602
Saveliy Stavitsky <s.stavitsky@icloud.com>
2023-06-12 19:08:42 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,8 @@ import PackageDescription
 let package = Package(
     name: "microecc",
     platforms: [
-        .iOS(.v9)
+        .iOS(.v12),
+        .macOS(.v10_14)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
